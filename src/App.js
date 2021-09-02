c2da99247a406ac3a0ec5407076bef1cc696d598
Gagi Shmagi <gagi.shmagi@gmail.com>
2021-09-02 15:39:54 +0300

    Added leaflet map custome component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import {Icon} from 'leaflet'
 import axios from 'axios'
 import { useState } from 'react';
 
+import LeafletMap from './Components/LeafletMap/Map'
+
+
 function App() {
 
   let data = {
@@ -52,18 +55,7 @@ function App() {
         <br />
         <button>submit</button>
       </form>
-
-        <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
-          <TileLayer
-            attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-          />
-          <Marker position={[51.505, -0.09]} icon={new Icon({iconUrl: markerIconPng, iconSize: [25, 41], iconAnchor: [12, 41]})} >
-            <Popup>
-              A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
-          </Marker>
-        </MapContainer>
+      <LeafletMap/>
       </header>
     </div>
   );
